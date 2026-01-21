ba887c8ea7bfac06b6d957f46f40e232c9b18093
yuvrajsinghh73638-tech <yuvrajsinghh73638@gmail.com>
2026-01-21 22:01:58 +0530

    Update bot.py
    .

--- bot.py
+++ bot.py
@@ -2,13 +2,10 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o")
+BOT_TOKEN = "8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o"
 
 if not BOT_TOKEN:
-    raise ValueError(import os
-# Line 8 ke aas-paas aisa kuch likhein
-TOKEN = "Aapka_Actual_Token_Yahan_Hoga" 
-)
+    raise ValueError("Bot Token nahi mila!")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
@@ -18,4 +15,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
+
 
