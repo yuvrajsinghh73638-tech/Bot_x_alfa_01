227cada184fdf3ae51c448a90ef0a36a6fc02381
yuvrajsinghh73638-tech <yuvrajsinghh73638@gmail.com>
2026-01-21 22:05:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o"
 if not BOT_TOKEN:
     raise ValueError("Bot Token nahi mila!")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running!")
