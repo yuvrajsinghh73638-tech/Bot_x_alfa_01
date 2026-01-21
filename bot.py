a421242613f315c4cbecbe66c32b0660c79203ff
yuvrajsinghh73638-tech <yuvrajsinghh73638@gmail.com>
2026-01-21 22:31:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# 8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o
 BOT_TOKEN = "8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o"
 
-if not BOT_TOKEN:
-    raise ValueError("Bot Token nahi mila!")
-
-
+# Is line mein bracket ke andar BOT_TOKEN hi rahega
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running!")
 
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
-
-
+if __name__ == "__main__":
+    app.run_polling()
+    
