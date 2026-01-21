f25744e41727a9959801804e8c7978b41ad1899e
yuvrajsinghh73638-tech <yuvrajsinghh73638@gmail.com>
2026-01-21 21:53:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 BOT_TOKEN = os.getenv("8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o")
 
 if not BOT_TOKEN:
-    raise ValueError("8529180151:AAES7AWR818cGYCd_DA3SrK4AE7ATcwR6_o")
+    raise ValueError(import os
+# Line 8 ke aas-paas aisa kuch likhein
+TOKEN = "Aapka_Actual_Token_Yahan_Hoga" 
+)
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
